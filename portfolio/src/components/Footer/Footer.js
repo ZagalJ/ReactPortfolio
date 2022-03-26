022fc665e85dcfab4623727cc56435c34dbee6ba
ZagalJ <jonathan.zagal@hotmail.com>
2022-03-26 14:22:36 -0500

    added footer and intro with sample text

--- portfolio/src/components/Footer/Footer.js
+++ portfolio/src/components/Footer/Footer.js
@@ -1,11 +1,26 @@
 import React from "react";
 import "./Footer.css";
+import {Container, Row, Col} from "react-bootstrap";
 
-
-const Footer =() => {
+function Footer(){
+    // we get todays date and use the year only
+    let date = new Date();
+    let year = date.getFullYear();
     return(
-        <h2>Footer test</h2>
+        <Container fluid className="footer">
+            <Row>
+                <Col md="4" className="footer-copywright">
+                    <div>Jonathan Zagal</div>
+                </Col>
+                <Col md="4" className="footer-copywright">
+                    <div> Copyright © {year}</div>
+                </Col>
+                <Col md="4" className="footer-body">
+                    <div>Social</div>
+                </Col>
+            </Row>
+        </Container>
     )
-};
+}
 
 export default Footer;
--- portfolio/src/components/intro/Intro.js
+++ portfolio/src/components/intro/Intro.js
@@ -4,7 +4,22 @@ import "./Intro.css";
 
 const Intro =() => {
     return(
-        <h2>Intro test</h2>
+        <div className="intro">
+            <div className="intro-left">
+                <div className="intro-left-wrapper">
+                    <h2 className="intro-hello">Hello, my name is</h2>
+                    <h1 className="intro-name">Jonathan</h1>
+                    <div className="intro-title">
+                        <h5 className="intro-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit dolor et nunc lobortis, vitae faucibus nisl convallis. Fusce congue ullamcorper tellus, et varius orci. In in convallis urna. Sed vitae lorem eu mi maximus euismod vitae nec tortor. Cras in orci in risus porttitor convallis quis ac ipsum. Integer at pulvinar tellus. Phasellus ultrices augue vehicula ante pulvinar, non suscipit felis tincidunt. Nunc vehicula velit metus, nec cursus enim aliquam et. Duis risus justo, egestas vitae neque vitae, sollicitudin elementum urna. Duis convallis interdum iaculis. Sed malesuada felis nisl, ac condimentum odio vehicula quis.
+                        </h5>
+                    </div>
+                </div>
+                <div className="intro-right">
+                <h1>my Pictures</h1>
+                </div>
+            </div>
+        </div>
     )
 };
 
